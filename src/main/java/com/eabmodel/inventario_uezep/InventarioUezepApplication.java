76508b220bda4ab175885fd777228b1a6631ba63
franxxdmsk132 <tapiafranklin666@gmail.com>
2024-03-30 17:57:01 -0500

    Creacion

---
+++ src/main/java/com/eabmodel/inventario_uezep/InventarioUezepApplication.java
@@ -0,0 +1,13 @@
+package com.eabmodel.inventario_uezep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioUezepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioUezepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eabmodel/inventario_uezep/InventarioUezepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eabmodel.inventario_uezep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioUezepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
