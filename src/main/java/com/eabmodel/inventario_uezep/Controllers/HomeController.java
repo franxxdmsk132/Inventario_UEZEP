1d279a07fd47004a5d6073e67e96197db8802a1c
franxxdmsk132 <tapiafranklin666@gmail.com>
2024-04-05 12:03:11 -0500

    Login con inicio simple de arreglar

---
+++ src/main/java/com/eabmodel/inventario_uezep/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.eabmodel.inventario_uezep.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+
+    @GetMapping
+    public String home() {
+
+        return "home/Inicio.html";
+    }
+}
