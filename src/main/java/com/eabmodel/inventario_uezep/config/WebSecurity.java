5f3cb32dbfa696171de200d65b40a15b6e12efb7
franxxdmsk132 <tapiafranklin666@gmail.com>
2024-04-05 11:45:03 -0500

    Login Acepta dos usuarios

--- src/main/java/com/eabmodel/inventario_uezep/config/WebSecurity.java
+++ src/main/java/com/eabmodel/inventario_uezep/config/WebSecurity.java
@@ -1,11 +1,22 @@
 package com.eabmodel.inventario_uezep.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+@Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("mamor").password("{noop}pollo").roles("manager")
+                .and()
+                .withUser("xd").password("{noop}xdnt").roles("manager");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception { 
         http
