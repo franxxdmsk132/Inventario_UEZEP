5e83e2a8fd267b73ecc64f9db0d08b4e550f41cb
franxxdmsk132 <tapiafranklin666@gmail.com>
2024-04-07 02:17:22 -0500

    R de muebles

--- src/main/java/com/eabmodel/inventario_uezep/Controller/MuebleController.java
+++ src/main/java/com/eabmodel/inventario_uezep/Controller/MuebleController.java
@@ -4,10 +4,8 @@ import com.eabmodel.inventario_uezep.Entity.Muebles;
 import com.eabmodel.inventario_uezep.Service.MuebleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,6 +24,6 @@ public class MuebleController {
     public String listaMuebles(Model model){
         List<Muebles> muebles = muebleService.findAll();
         model.addAttribute("muebles",muebles);
-        return "muebles/list-muebles";
+        return "muebles/lista-muebles";
     }
 }
